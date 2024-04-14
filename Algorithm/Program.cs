bcc7c9a072e71389831d1263726e1f8943199d24
MaksItLab <maksim.ardintsev@mail.ru>
2024-04-14 16:21:03 +0300

    Добавлено размещение нескольких соединений на 1-ой магистрали

--- Algorithm/Program.cs
+++ Algorithm/Program.cs
@@ -23,14 +23,16 @@ namespace Algorithm
 
 			crystal.Scheme.Components = new List<Component>
 			{
-				new Component {ComponentId = 1, Name="cpu1t", Position = new Position {X = 1, Y = 1 }, ConnectionComponentId  = 6},
-				new Component {ComponentId = 2, Name="cpu4b", Position = new Position {X = 2, Y = -1 }, ConnectionComponentId  = 8},
-				new Component {ComponentId = 3, Name="cpu3t", Position = new Position {X = 3, Y = 1 }, ConnectionComponentId  = 7},
-				new Component {ComponentId = 4, Name="cpu2b", Position = new Position {X = 4, Y = -1 }, ConnectionComponentId  = 5},
-				new Component {ComponentId = 5, Name="cpu2t", Position = new Position {X = 6, Y = 1 }, ConnectionComponentId  = 4},
-				new Component {ComponentId = 6, Name="cpu1b", Position = new Position {X = 11, Y = -1 }, ConnectionComponentId  = 1},
-				new Component {ComponentId = 7, Name="cpu3t", Position = new Position {X = 11, Y = 1 }, ConnectionComponentId  = 3},
-				new Component {ComponentId = 8, Name="cpu4t", Position = new Position {X = 18, Y = 1 }, ConnectionComponentId  = 2},
+				new Component {ComponentId = 1, Name="cpu1t", Position = new Position {X = 1, Y = 1 }, ConnectionComponentId  = 7},
+				new Component {ComponentId = 2, Name="cpu4b", Position = new Position {X = 2, Y = -1 }, ConnectionComponentId  = 10},
+				new Component {ComponentId = 3, Name="cpu3t", Position = new Position {X = 3, Y = 1 }, ConnectionComponentId  = 8},
+				new Component {ComponentId = 4, Name="cpu2b", Position = new Position {X = 4, Y = -1 }, ConnectionComponentId  = 6},
+				new Component {ComponentId = 5, Name="cpu5t", Position = new Position {X = 14, Y = 1 }, ConnectionComponentId  = 9},
+				new Component {ComponentId = 6, Name="cpu2t", Position = new Position {X = 6, Y = 1 }, ConnectionComponentId  = 4},
+				new Component {ComponentId = 7, Name="cpu1b", Position = new Position {X = 11, Y = -1 }, ConnectionComponentId  = 1},
+				new Component {ComponentId = 8, Name="cpu3t", Position = new Position {X = 11, Y = 1 }, ConnectionComponentId  = 3},
+				new Component {ComponentId = 9, Name="cpu5b", Position = new Position {X = 17, Y = -1 }, ConnectionComponentId  = 5},
+				new Component {ComponentId = 10, Name="cpu4t", Position = new Position {X = 18, Y = 1 }, ConnectionComponentId  = 2},
 			};
 
 			for (int i = 0; i < crystal.countOfMagistrals; i++)
